17c74f2e48440feb1641d7dfe8a1b1b71a52482e
Niklas Paasonen <n.paasonen@yahoo.com>
2023-02-22 11:27:55 +0100

    remove show_entities_html evaluation

--- py_scripts/ner_util/evaluation.py
+++ py_scripts/ner_util/evaluation.py
@@ -1,4 +1,3 @@
-from IPython.core.display import display, HTML
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
@@ -71,73 +70,6 @@ def show_entities(ner_system, sentence):
         if last_tag in labels:
             output += f" </{last_tag}> "
         print(output)
-            
-
-def show_entities_html(tagger, sentences):
-    tagged_sentences = tagger.predict(sentences)
-    if tagger.bert_tokenizer is not None:
-        word_encoded = tagger.bert_tokenizer(sentences, is_split_into_words=True, truncation=True, 
-                                             max_length=tagger.params.bert_max_len).input_ids
-        sentences = [[tagger.bert_tokenizer.decode(i) for i in s[1:-1]] for s in word_encoded]
-    
-    labels = ['First_Name', 'Last_Name', 'Phone_Number', 'Age', 'Full_Date', 'Date_Part', 'Health_Care_Unit', 'Location']
-
-    styles = {
-        'First_Name': 'background-color: #aaffaa; color: black;',
-        'Last_Name': 'background-color: #aaaaff; color: black;',
-        'Phone_Number': 'background-color: #ff8800; color: black;',
-        'Age': 'background-color: #00ffff; color: black;',
-        'Full_Date': 'background-color: #ff3333; color: white;',
-        'Date_Part': 'background-color: #44bbff; color: white;',
-        'Health_Care_Unit': 'background-color: #308227; color: white;',
-        'Location': 'background-color: #308227; color: white;'
-
-    }
-    content = ['<div style="font-size:150%; line-height: 150%;">']
-
-    for tokens, tags in zip(sentences, tagged_sentences):
-        content.append('<div>')
-        current_entity = None
-        for token, tag in zip(tokens, tags):
-            if tag[0] not in ['B', 'I']:
-                if current_entity:
-                    content.append('</b>')
-                    current_entity = None
-                content.append(' ')
-            elif tag[0] == 'B':
-                if current_entity:
-                    content.append('</b>')
-                content.append(' ')
-                current_entity = tag[2:]
-                content.append(f'<b style="{styles[current_entity]} border-radius: 3px; padding: 3px;">')
-                content.append(f'<sup style=font-size:small;><tt>{current_entity}</tt></sup> ')
-
-            else:
-                entity = tag[2:]
-                if entity == current_entity:
-                    content.append(' ')
-                elif current_entity is None:
-                    content.append(' ')
-                    content.append('<sup style=font-size:small;><tt>[ERROR]</tt></sup> ')
-                    content.append(f'<b style="{styles[entity]} border-radius: 3px; padding: 3px;">')
-                    content.append(f'<sup style=font-size:small;><tt>{entity}</tt></sup> ')
-                else:
-                    content.append('</b>')
-                    content.append(' ')
-                    content.append('<sup style=font-size:small;><tt>[ERROR]</tt></sup> ')
-                    content.append(f'<b style="{styles[entity]} border-radius: 3px; padding: 3px;">')
-                    content.append(f'<sup style=font-size:small;><tt>{entity}</tt></sup> ')
-                current_entity = entity
-            content.append(token)
-        if current_entity:
-            content.append('</b>')
-        content.append('</div>')
-    content.append('</div>')    
-    html = ''.join(content).strip()
-    display(HTML(html))
-    with open("entities.png", "wb") as png:
-        png.write(HTML(html))
-
 
 def plot_training(ner_system):
   fig, ax = plt.subplots(1, 2, figsize=(2*6,1*6))
