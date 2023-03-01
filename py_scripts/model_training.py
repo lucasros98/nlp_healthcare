bd089c01943693d3d96c9bea60f4c39d7a4169c7
Lucas Rosvall <lucasrosvall@gmail.com>
2023-03-01 13:45:51 +0100

    Updates

--- py_scripts/model_training.py
+++ py_scripts/model_training.py
@@ -1,15 +1,19 @@
+#Given a model and a set of parameters, train the model and return the results
+#Run the model multiple times and average the results
 import pandas as pd
 import os
 import sys
 from torch import nn
 from dotenv import load_dotenv, find_dotenv
+
 load_dotenv(find_dotenv())
 
+from data import get_training_data
 import ner_util.ner_system as ner_util
 
-def run_model(params, model, tokenizer, zipped, runs=5):
+def run_model(params, model, tokenizer, precentage=100, lang='swe',runs=5,uncased=False):
     #Get the data (X_train, Y_train, X_val, Y_val, X_test, Y_test)
-    X_train, Y_train, X_val, Y_val, X_test, Y_test = zipped
+    X_train, Y_train, X_val, Y_val, X_test, Y_test = get_training_data(precentage,lang,uncased)
   
     #results
     results = []
@@ -18,11 +22,13 @@ def run_model(params, model, tokenizer, zipped, runs=5):
     for i in range(runs):
         print("Run: ", i)
 
-        #update the seed
+        #Update the seed
         params["random_seed"] = i
 
-        #Fit the model
+        #Create a new instance of the model
         ner_system = ner_util.SequenceLabeler(params, model, bert_tokenizer=tokenizer)
+        
+        #Fit the model
         ner_system.fit(X_train, Y_train, X_val, Y_val)
 
         #Get the evaluation results
@@ -35,11 +41,3 @@ def run_model(params, model, tokenizer, zipped, runs=5):
     results = pd.DataFrame(results)
     results = results.mean(axis=0)
     return results
-
-    
-
-
-
-
-
-
