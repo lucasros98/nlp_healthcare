1ad0fa4bbcdf8277aae908d2d7986ac968951e48
Niklas Paasonen <n.paasonen@yahoo.com>
2023-03-07 11:33:15 +0100

    add random_deletion method to data_aug

--- py_scripts/augmentation.py
+++ py_scripts/augmentation.py
@@ -19,6 +19,10 @@ def data_augmentation(X, Y, aug_type):
                 sentence_new, labels_new = synonym_replacement(sentence, labels)
                 X_new.append(sentence_new)
                 Y_new.append(labels_new)
+            elif aug_type == "random_deletion":
+                sentence_new, labels_new = random_deletion(sentence, labels)
+                X_new.append(sentence_new)
+                Y_new.append(labels_new)
     return X_new, Y_new
 
 def back_translation(X,Y):
@@ -30,6 +34,18 @@ def back_translation(X,Y):
     write_csv_file("back_translation", X_new, Y_new, "augmented")
         
     return X_new, Y_new
+
+def random_deletion(sentence, labels):
+    dist = random.binomial(n=1, p=binomial_p, size=len(sentence))
+    new_sentence = []
+    new_labels = []
+    for word, label, prob in zip(sentence, labels, dist):
+        if prob == 1 and label == 'O':
+            continue
+        else:
+            new_sentence.append(word)
+            new_labels.append(label)
+    return(new_sentence, new_labels)
 
 def synonym_replacement(sentence, labels):
     generator = LabelGenerator()
@@ -61,7 +77,7 @@ print("len:", len(data[0]))
 for col in data:
     for row in col:
         print(row)
-new_data = data_augmentation(data[0], data[1], "synonym_replacement")
+new_data = data_augmentation(data[0], data[1], "random_deletion")
 print("len:", len(new_data[0]))
 for col in new_data:
     for row in col:
