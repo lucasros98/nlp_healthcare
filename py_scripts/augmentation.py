6965ee5704adca4e160f9b4d80d00c2ec6ae5245
Niklas Paasonen <n.paasonen@yahoo.com>
2023-03-08 13:41:47 +0100

    add data fetching to augmentation

--- py_scripts/augmentation.py
+++ py_scripts/augmentation.py
@@ -5,15 +5,29 @@ from numpy import random
 from file_handler import read_csv_file, write_csv_file
 from generation import LabelGenerator
 from tqdm import tqdm
+from data import get_training_data
+import os
+import sys
+from dotenv import load_dotenv,find_dotenv
 
-binomial_p = 1
+sys.path.append(os.path.dirname(find_dotenv()))
+load_dotenv(find_dotenv())
+
+#Get the path for the data
+PATH = os.getenv('DATA_PATH')
+
+binomial_p = 0.3
 num_new_docs = 1
 
-def data_augmentation(X, Y, aug_type):
-    X_new = X.copy()
-    Y_new = Y.copy()
+def data_augmentation(aug_type):
+    X_train,Y_train,X_val,Y_val,X_test,Y_test = get_training_data(precentage=25,uncased=False)
+    X_new = X_train.copy()
+    Y_new = Y_train.copy()
 
-    for sentence, labels in tqdm(zip(X,Y), desc=f'aug_type={aug_type} with p={binomial_p}, num_new_docs={num_new_docs}'):
+    print('Start: X_train length: ', len(X_train))
+    print('Start: Y_train length: ', len(Y_train))
+
+    for sentence, labels in tqdm(zip(X_train,Y_train), desc=f'aug_type={aug_type} with p={binomial_p}, num_new_docs={num_new_docs}'):
         for i in range(num_new_docs):
             if aug_type == "synonym_replacement":
                 sentence_new, labels_new = synonym_replacement(sentence, labels)
@@ -27,9 +41,22 @@ def data_augmentation(X, Y, aug_type):
                 sentence_new, labels_new = mention_replacement(sentence, labels)
                 X_new.append(sentence_new)
                 Y_new.append(labels_new)
-    return X_new, Y_new
+    
 
-    # TODO: remove duplicates...
+    print('End: X_train length before duplicates removed: ', len(X_new))
+    print('End: Y_train length: before duplicates removed', len(Y_new))
+
+    # remove duplicates from X_new and Y_new
+    X_new = list(dict.fromkeys(X_new))
+    Y_new = list(dict.fromkeys(Y_new))
+
+    print('End: X_train length after duplicates removed: ', len(X_new))
+    print('End: Y_train length: after duplicates removed', len(Y_new))
+
+    #Write the new data to a csv file
+    write_csv_file(aug_type, X_new, Y_new, "augmented")
+
+    return X_new, Y_new
 
 def back_translation(X,Y):
     #Back translate the text
@@ -117,20 +144,5 @@ def synonym_replacement(sentence, labels):
     return(new_sentence, new_labels)
             
 
-data = [
-    ['ansvarig', 'vid', 'inskrivning', 'är', 'ssk', 'Kim', 'Dahlström', 'Johansson'],
-    ['besök', 'mamma', 'Lucas', 'på', 'morgonen']
-], [
-    ['O', 'O', 'O', 'O', 'O', 'B-First_Name', 'B-Last_Name', 'I-Last_Name'],
-    ['O', 'O', 'B-First_Name', 'O', 'O']
-]
 
-print("len:", len(data[0]))
-for col in data:
-    for row in col:
-        print(row)
-new_data = data_augmentation(data[0], data[1], "mention_replacement")
-print("len:", len(new_data[0]))
-for col in new_data:
-    for row in col:
-        print(row)
+data_augmentation("synonym_replacement")
