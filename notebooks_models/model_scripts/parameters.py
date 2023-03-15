33b28f14ef38d6c649a26fc0ad187e6db9350452
Lucas Rosvall <lucasrosvall@gmail.com>
2023-03-15 11:20:39 +0100

    changed params

--- notebooks_models/model_scripts/parameters.py
+++ notebooks_models/model_scripts/parameters.py
@@ -1,33 +1,36 @@
 import torch
 
 class NERParameters():
+    def __init__(self):
+        # Random seed, for reproducibility.
+        self.random_seed = 1
 
-    # Random seed, for reproducibility.
-    random_seed = 1
-    
-    # cuda or cpu
-    device = "cuda" if torch.cuda.is_available() else "cpu"   
-    
-    #Tagging scheme used in data: IO or BIO (Inside-Outside-Beginning)
-    tagging_scheme="BIO"
-    
-    #Training parameters
-    n_epochs = 25
-    batch_size = 32
+        
+        # cuda or cpu
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"   
+        
+        #Tagging scheme used in data: IO or BIO (Inside-Outside-Beginning)
+        self.tagging_scheme="BIO"
+        
+        #Training parameters
+        self.n_epochs = 25
+        self.batch_size = 32
 
-    learning_rate = 2e-5
-    epsilon = 1e-08
-    weight_decay = 0.0
+        self.learning_rate = 2e-5
+        self.epsilon = 1e-08
+        self.weight_decay = 0.0
 
-    #Early stopping parameters
-    early_stopping = True
-    patience = 5
+        #Early stopping parameters
+        self.early_stopping = True
+        self.patience = 5
 
-    #For linear learning rate decay
-    lr_decay = True
-    warmup_steps = 0.1 # Warmup steps (In precentage)
+        #For linear learning rate decay
+        self.lr_decay = True
+        self.warmup_steps = 0.1 # Warmup steps (In precentage)
 
-    # Word dropout rate.
-    word_dropout_prob = 0
+        #Augmented
 
-    bert_max_len = 512
+        # Word dropout rate.
+        self.word_dropout_prob = 0
+
+        self.bert_max_len = 512
