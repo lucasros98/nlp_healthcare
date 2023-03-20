1e69600ad64a28d4db113793c240e085dec1e4b2
Niklas Paasonen <n.paasonen@yahoo.com>
2023-03-20 09:49:42 +0100

    add Logger class

---
+++ py_scripts/ner_util/logger.py
@@ -0,0 +1,18 @@
+import wandb
+
+class Logger:
+    """Wandb logger for tracking model training and evaluation."""
+    
+    def __init__(self, project, config):
+        # start a new wandb run to track this script
+
+        wandb.init(
+            # set the wandb project where this run will be logged
+            project=project,
+            # track hyperparameters and run metadata
+            config=config
+        )
+    
+    def __call__(self, values):
+        # log the values to wandb (e.g. {"acc": acc, "loss": loss})
+        wandb.log(values)
