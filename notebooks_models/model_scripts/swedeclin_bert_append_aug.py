7ee7e4a93db9dce93e10377989c24fd97a99383a
Niklas Paasonen <n.paasonen@yahoo.com>
2023-04-26 11:44:03 +0200

    add run scripts for stack & append aug

---
+++ notebooks_models/model_scripts/swedeclin_bert_append_aug.py
@@ -0,0 +1,55 @@
+import os
+import sys
+import pandas as pd
+from torch import nn
+from dotenv import load_dotenv, find_dotenv
+sys.path.append(os.path.dirname(find_dotenv()))
+load_dotenv(find_dotenv())
+
+from py_scripts.model_training import run_model
+
+#Get the precentage from the sys args
+try:
+    precentage = float(float(sys.argv[1])) if len(sys.argv) > 1 and sys.argv[1] != "None" else 100
+except:
+    precentage = 100
+    print("Error occured while parsing the precentage from the sys args. Please check the sys args. Using {}% of the data.".format(precentage))
+
+combinations = [
+    [
+        {
+            'aug_method': 'unique_mention_replacement',
+            'p': 0.5,
+            'num_new_docs': 3,
+            'data_size': precentage,
+            'bt_type': None
+        },
+        {
+            'aug_method': 'local_mention_replacement',
+            'p': 0.3,
+            'num_new_docs': 1,
+            'data_size': precentage,
+            'bt_type': None
+        }
+    ],
+    [
+        {
+            'aug_method': 'unique_mention_replacement',
+            'p': 0.5,
+            'num_new_docs': 2,
+            'data_size': precentage,
+            'bt_type': None
+        },
+        {
+            'aug_method': 'synonym_replacement',
+            'p': 0.1,
+            'num_new_docs': 1,
+            'data_size': precentage,
+            'bt_type': None
+        }
+    ]
+]
+
+for aug_params_list in combinations:
+    #Run the model
+    run_model(model_name="swedeclin_bert",bert_model="/mimer/NOBACKUP/groups/snic2021-23-309/project-data/nlp-for-healthcare/SweDeClin-BERT_copy/",local_files_only=True,precentage=precentage,lang='sv',runs=5,aug_params_list=aug_params_list, aug_combination='append')
